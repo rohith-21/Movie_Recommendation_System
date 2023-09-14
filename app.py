fed422745c48215d8361ab0dfbe8f39f9b843065
rohith-21 <55896422+rohith-21@users.noreply.github.com>
2023-09-15 00:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,3 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-st.text('\n')
-st.text('\n')
-st.text('\n')
-st.text('--------------------------MADE BY CHIDURALA NIKHIL-------------------------')
